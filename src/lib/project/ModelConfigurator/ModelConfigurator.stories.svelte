fab46ee196bebe0510abe5af7ee1a0c969cb65a9
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-06-19 12:30:49 +0530

    model page

--- src/lib/project/ModelConfigurator/ModelConfigurator.stories.svelte
+++ src/lib/project/ModelConfigurator/ModelConfigurator.stories.svelte
@@ -1,17 +1,18 @@
 <script context="module" lang="ts">
-	import { Story, Template } from '@storybook/addon-svelte-csf';
-	import ModelConfigurator from './ModelConfigurator.svelte';
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+  import ModelConfigurator from "./ModelConfigurator.svelte";
 
-	let src = '/models/model.gltf';
-	let texture = '/models/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWN.webp';
-	export const meta = {
-		title: 'ModelConfigurator',
-		component: ModelConfigurator
-	};
+  let src = "/models/model.gltf";
+  let texture =
+    "/models/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWN.webp";
+  export const meta = {
+    title: "Components/Project/ModelConfigurator",
+    component: ModelConfigurator,
+  };
 </script>
 
 <Template let:args>
-	<ModelConfigurator {...args} bind:texture></ModelConfigurator>
+  <ModelConfigurator {...args} bind:texture></ModelConfigurator>
 </Template>
 
 <Story name="Model" args={{}} />
