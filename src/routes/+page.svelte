61c59305647821e1ccc4a0d567b21255d0a3ca2f
Gourav Saini <gouravsaini9797@gmail.com>
2024-06-20 16:33:56 +0530

    feat: Landing-page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   // import { browser } from "$app/environment";
   // import { VideoPlayer } from "$lib/project";
-  import TextCard from "$lib/project/TextCard/TextCard.svelte";
-  import { Typography, Button } from "$lib/ui";
+  import { TextCard, Carousel } from "$lib/project";
+  import { Typography, Button, Input } from "$lib/ui";
+
+  let name: string;
+  let email: string;
+  let phone: number;
+  let message: string;
 </script>
 
 <main>
@@ -57,35 +62,38 @@
           }}
         />
         <TextCard
-          args={{ variant: "default" }}
+          args={{ variant: "listCard" }}
           l10n={{
-            heading: "Our Approach",
-            subHeading: "A Real Estate Company for a purpose!",
+            heading: "Why Choose SPS Realty:",
             content: [
-              "We dream of coming up with innovation, strategically designed to enhance the thriving residential hub. We seek to match up the lifestyle of the entire area, becoming a centre for social and economic growth. Our aim always revolves around one entity that is our customers.",
-              "Ever since our parent company SP Singla Constructions was initiated, we have moved forward with every new step trying to meet their needs, ideas, and demands. The same mythology we aim to practise here.  We believe in more than just buying or selling the properties! Our main objective lies in creating homes and enhancing communities.",
+              "We are here to set up a benchmark in the world of Real Estate!",
+              "Meet Our Team who aim to be a Real Estate Advisor with a knack for meeting every client’s need, each time.",
+              "Our team will ensure that the property garners the major returns on the investment.",
+              "Our experts are the straight shooters and know exactly what price a home will sell for and what needs to be done to result in the maximum profits.",
+              "More importantly! Our Expert Team will never try to sell something to you that is not just well. We would ensure to become a golden ticket for our clients.",
             ],
           }}
         />
         <TextCard
           args={{ variant: "default" }}
           l10n={{
-            heading: "Our Approach",
-            subHeading: "A Real Estate Company for a purpose!",
+            heading: "Our Mission:",
+            subHeading: "A Real Estate Solutions Company",
             content: [
-              "We dream of coming up with innovation, strategically designed to enhance the thriving residential hub. We seek to match up the lifestyle of the entire area, becoming a centre for social and economic growth. Our aim always revolves around one entity that is our customers.",
-              "Ever since our parent company SP Singla Constructions was initiated, we have moved forward with every new step trying to meet their needs, ideas, and demands. The same mythology we aim to practise here.  We believe in more than just buying or selling the properties! Our main objective lies in creating homes and enhancing communities.",
+              "Our mission is to ensure real service that is not measured or bought with money but with sincerity and integrity. SPS Realty is a professional, full-service real estate solutions firm that buys and sells properties. We specialise in buying homes, renovations, and reselling to real home buyers and landlords.",
+              "At SPS Realty, our team is immensely motivated, informative, ethical, and full of resources. Our dedicated staff is expertise in committing to helping people with their real estate requirements and ensuring successful deals. Our sincere staff has the integrity to follow up on the commitments and work sincerely to navigate any transaction to make sure you are entirely informed for making the best decision in terms of perfect homes.",
             ],
           }}
         />
         <TextCard
           args={{ variant: "default" }}
           l10n={{
-            heading: "Our Approach",
-            subHeading: "A Real Estate Company for a purpose!",
+            heading: "Our Vision",
+            subHeading:
+              "Trusted by over 5 million users all across 10 countries",
             content: [
-              "We dream of coming up with innovation, strategically designed to enhance the thriving residential hub. We seek to match up the lifestyle of the entire area, becoming a centre for social and economic growth. Our aim always revolves around one entity that is our customers.",
-              "Ever since our parent company SP Singla Constructions was initiated, we have moved forward with every new step trying to meet their needs, ideas, and demands. The same mythology we aim to practise here.  We believe in more than just buying or selling the properties! Our main objective lies in creating homes and enhancing communities.",
+              "We desire to empower dreams and build legacies! The main vision at SPS Realty is to be an unmeasurable leader in the field of real estate. We aim to be recognized for transforming lives through exceptional service, best solutions, and a refined commitment to extremely sustainable and thriving communities. For us property is not just a space to live, but a space that transcends bricks and mortar bringing up dreams, and memories and also enhancing value.",
+              "Our main vision is to ensure every client with not just a property, but a place to call home! We believe in setting an impression on the landscape we serve. We focus not only on building structures but also on setting a benchmark in the world of real estate!",
             ],
           }}
         />
@@ -106,20 +114,20 @@
         </div>
         <div class="project-card">
           <Typography type="body" _color="var(--secondary-700)"
-            >Projects Completed</Typography
+            >Awards Won</Typography
           >
           <br />
           <Typography type="heading" _color="var(--secondary-900)"
-            >15</Typography
+            >25</Typography
           >
         </div>
         <div class="project-card">
           <Typography type="body" _color="var(--secondary-700)"
-            >Projects Completed</Typography
+            >Ongoint Projects</Typography
           >
           <br />
           <Typography type="heading" _color="var(--secondary-900)"
-            >15</Typography
+            >12</Typography
           >
         </div>
       </div>
@@ -132,6 +140,63 @@
           ></Typography
         >
       </div>
+    </div>
+  </section>
+  <section class="gallery">
+    <div class="container">
+      <div class="section-header">
+        <Typography type="heading" _color="var(--secondary-900)"
+          >Our Latest Projects</Typography
+        >
+        <img src="/images/Home/HeliosLogo.svg" alt="logo" />
+      </div>
+      <Carousel
+        src={[
+          "/images/Home/GalleryImageOne.png",
+          "/images/Home/GalleryImageTwo.png",
+          "/images/Home/GalleryImageThree.png",
+          "/images/Home/GalleryImageFour.png",
+          "/images/Home/GalleryImageFive.png",
+        ]}
+      />
+    </div>
+  </section>
+  <section class="form">
+    <div class="container">
+      <form>
+        <Typography type="heading" _color="var(--secondary-900)"
+          >Get in touch</Typography
+        >
+        <div class="spacer" aria-hidden="true"></div>
+        <Typography type="body" _color="var(--secondary-900)"
+          >Our Team would love to hear from you</Typography
+        >
+        <br />
+        <Input
+          args={{ variant: "text" }}
+          l10n={{ label: "Name", placeholder: "Marvin McKinney" }}
+          bind:value={name}
+        />
+        <br />
+        <Input
+          args={{ variant: "email" }}
+          l10n={{ label: "Email", placeholder: "xyz@example.com" }}
+          bind:value={email}
+        />
+        <br />
+        <Input
+          args={{ variant: "mobile" }}
+          l10n={{ label: "Phone", placeholder: "+91 0000000000" }}
+          bind:value={phone}
+        />
+        <br />
+        <Input
+          args={{ variant: "textarea" }}
+          l10n={{ label: "Message", placeholder: "Write a message" }}
+          bind:value={message}
+        />
+      </form>
+      <img src="/images/Home/GridImage.png" alt="" />
     </div>
   </section>
 </main>
@@ -182,6 +247,7 @@
     }
     & > .projects {
       margin-block-start: 2.5rem;
+      margin-block-end: 5rem;
       & > .container {
         max-inline-size: 1280px;
         inline-size: 90vw;
@@ -196,6 +262,10 @@
           }
           & > .project-card {
             inline-size: 33%;
+            @media screen and (width <= 768px) {
+              inline-size: 90vw;
+              margin-block-end: 2.5rem;
+            }
             background-color: var(--primary-700);
             text-align: center;
             padding-inline: 80px;
@@ -211,6 +281,40 @@
           margin-block-start: 2.5rem;
           text-align: center;
           padding-block: 53px;
+        }
+      }
+    }
+    & > .gallery {
+      margin-block-end: 5rem;
+      background-color: var(--primary-700);
+      padding-block: 64px;
+      & > .container {
+        max-inline-size: 1280px;
+        inline-size: 90vw;
+        margin-inline: auto;
+        & > .section-header {
+          text-align: center;
+          margin-block-end: 2.5rem;
+        }
+      }
+    }
+    & > .form {
+      margin-block-end: 5rem;
+      background-color: var(--primary-700);
+      padding-block: 64px;
+      & > .container {
+        max-inline-size: 1280px;
+        inline-size: 90vw;
+        margin-inline: auto;
+        display: grid;
+        grid-template-columns: auto auto;
+        align-items: center;
+        gap: 80px;
+        @media screen and (width <= 768px) {
+          grid-template-columns: auto;
+        }
+        & > img {
+          inline-size: 100%;
         }
       }
     }
