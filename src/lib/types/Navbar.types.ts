3d6b52e0bec3e9b43ada1b78177db0a5493cfa9c
yashrajbharti <y.bharti@cybtekk.com>
2024-06-20 17:37:52 +0530

    fix

--- src/lib/types/Navbar.types.ts
+++ src/lib/types/Navbar.types.ts
@@ -1,14 +1,12 @@
 export interface INavbarContent {
   navOptions: ISingleNavOption[];
 }
-
 interface ISingleNavOption {
   name: string;
   href: string;
   target?: string;
   // isSubOptions: boolean;
 }
-
 export interface INavbarHandlers {
   handleContact: () => void;
 }
