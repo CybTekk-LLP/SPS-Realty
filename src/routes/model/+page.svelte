9402b41d904cfa2024477f57593b8f208b31aa87
yashrajbharti <y.bharti@cybtekk.com>
2024-06-20 15:50:06 +0530

    model page

--- src/routes/model/+page.svelte
+++ src/routes/model/+page.svelte
@@ -2,4 +2,9 @@
   import { ModelConfigurator } from "$lib/project";
 </script>
 
-<ModelConfigurator src="/models/scene.gltf"></ModelConfigurator>
+<div style="text-align: center;">
+  <img src="/images/Models/Helios.svg" alt="Helios Project" height="100px" />
+  <br /><br />
+  <ModelConfigurator src="/models/scene.gltf"></ModelConfigurator>
+  <br /><br /><br /><br />
+</div>
