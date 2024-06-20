1cfc5bbc717bd60e2f8fcfc4a27566f0e8393835
yashrajbharti <y.bharti@cybtekk.com>
2024-06-20 12:22:21 +0530

    card

---
+++ src/lib/project/ProjectCard/ProjectCard.stories.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+    import { Story, Template } from "@storybook/addon-svelte-csf";
+    import ProjectCard from "./ProjectCard.svelte";
+
+    export const meta = {
+      title: "Components/Project/ProjectCard",
+      component: ProjectCard,
+    };
+  </script>
+  
+  <Template let:args>
+    <ProjectCard {...args} content={{src:"https://picsum.photos/700"}}></ProjectCard>
+  </Template>
+  
+  <Story name="card" args={{}} />
+  
