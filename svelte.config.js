ffc2dfdbe0832a0a97308703d8e98a2844cb3488
yashrajbharti <y.bharti@cybtekk.com>
2024-07-18 12:18:07 +0530

    fix: deployment

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,8 @@ import staticAdapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const adapter = staticAdapter({ fallback: "index.html" });
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: [
     vitePreprocess(),
     preprocess({
@@ -15,14 +11,13 @@ const config = {
     }),
   ],
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter,
-    files: {
-      assets: "./src/static",
+    adapter: staticAdapter({
+      // Set fallback to '200.html' if using SPA mode, or 'index.html' otherwise
+      fallback: "index.html",
+    }),
+    paths: {
+      base: '/SPS-Realty', // Adjust this to your repo name
     },
-    outDir: "./.svelte-kit",
     prerender: {
       handleHttpError: "warn",
     },
